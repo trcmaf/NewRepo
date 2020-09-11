d38b1842d0dfc775acc70d1919bbb46a4e84767d
trcmaf <student@C251U36>
2020-09-11 15:22:08 +0300

    init

---
+++ prac/01-02-prac/pr01-02-upr2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace pr01_02_upr2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string s = "Microsoft .NET Framework 2 Application Development Foundation";
+      string[] sa = s.Split(" ");
+      Array.Sort(sa);
+      s = string.Join(" ", sa); 
+      Console.WriteLine(s);
+    }
+  }
+}
