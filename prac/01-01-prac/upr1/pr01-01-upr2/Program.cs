846c4438f9560fcf9bb70a7163f234ec1039d329
trcmaf <student@C251U36>
2020-09-11 13:55:10 +0300

    init

---
+++ prac/01-01-prac/upr1/pr01-01-upr2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace pr01_01_upr1
+{
+  struct Person
+  {
+    public string firstName;
+    public string lastName;
+    public int age;
+    public Genders gender;
+    public enum Genders : int { Male, Female };
+    public Person(string _firstName, string _lastName, int _age, Genders _gender)
+    {
+      firstName = _firstName;
+      lastName = _lastName; 
+      age = _age;
+      gender = _gender;
+    }
+    public override string ToString()
+    {
+      return firstName + " " + lastName + " (" + gender + "), age " + age;
+    }
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Person p = new Person("Tony", "Allen", 32, Person.Genders.Male);
+      Console.WriteLine(p.ToString());
+    }
+  }
+}
