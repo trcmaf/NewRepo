93a6af730440a53bf13375dccaa2ac23100f48e4
trcmaf <student@C251U36>
2020-09-11 13:28:43 +0300

    fix 1

---
+++ prac/01-01-prac/upr1/pr01-01-upr1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace pr01_01_upr1
+{
+  struct Person
+  {
+    public string firstName;
+    public string lastName;
+    public int age;
+    public Person(string _firstName, string _lastName, int _age)
+    {
+      firstName = _firstName;
+      lastName = _lastName; age = _age;
+    }
+    public override string ToString()
+    {
+      return firstName + " " + lastName + ", age " + age;
+    }
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Person p = new Person("Tony", "Allen", 32); 
+      Console.WriteLine(p);
+    }
+  }
+}
