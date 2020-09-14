a3228461d0f9bd70b44fe54ff56ea786127bc7eb
trcmaf <anna.v.sokolova20@gmail.com>
2020-09-14 19:55:38 +0300

    init

---
+++ practice/01-practice/practicum-02-upr2/practicum-02-upr2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace practicum_02_upr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Microsoft .NET Framework 2 Application Development Foundation";
+            string[] sa = s.Split(" ");
+            Array.Sort(sa);
+            s = string.Join(" ", sa); 
+            Console.WriteLine(s);
+
+        }
+    }
+}
