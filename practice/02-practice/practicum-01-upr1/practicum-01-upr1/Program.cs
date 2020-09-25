40e4df3e699b6eafb278e661fe4554da7a2a5475
trcmaf <student@C251U37>
2020-09-25 14:21:29 +0300

    init

---
+++ practice/02-practice/practicum-01-upr1/practicum-01-upr1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace practicum_01_upr1
+{
+
+  class Program
+  {
+    static void ShowDirectory(Directorylnfo dir)
+    {
+      foreach (Filelnfo file in dir.GetFilesO)
+      {
+        Console.WriteLine("File: {0}", file.FullName);
+      }
+      foreach (Directorylnfo subDir in dir.GetDirectoriesO)
+      {
+        ShowDirectory(subDir);
+      }
+    }
+    static void Main(string[] args)
+    {
+      Directorylnfo dir = new Directorylnfo(Environment.SystemDirectory); 
+      ShowDirectory(dir);
+    }
+  }
+}
