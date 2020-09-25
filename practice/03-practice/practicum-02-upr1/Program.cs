db480eda960241a1c36e818c52f3388bb8f98f92
trcmaf <student@C251U37>
2020-09-25 14:53:59 +0300

    init

---
+++ practice/03-practice/practicum-02-upr1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace practicum_02_upr1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      StreamReader sr = new StreamReader(@"boot.ini");
+      StreamWriter sw = new StreamWriter(”boot - utf7.txt", false, Encoding.UTF7);
+      sw.WriteLine(sr.ReadToEnd());
+      sw.Close();
+      sr.Close();
+    }
+  }
+}
