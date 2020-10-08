fea6fba41fe20643d7ca1c2e3e3fa44552dbd866
trcmaf <anna.v.sokolova20@gmail.com>
2020-10-08 18:29:53 +0300

    init

---
+++ practice/04-practice/practicum-04-upr1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Collections;
+using System.Collections.Specialized;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ListDictionary list = new ListDictionary(new CaseInsensitiveComparer(CultureInfo.InvariantCulture));//Словарь, нечувствительный к регистру
+        
+        list["Estados Unidos"] = "United States of America";
+        list["Canada"] = "Canada";
+        list["Espana"] = "Spain";
+        
+        Console.WriteLine(list["espana"]);
+        Console.WriteLine(list["CANADA"]);
+        Console.Read();
+    }
+}
