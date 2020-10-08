75fdfac992ab8b4d1bbe6e21aa8dc27c2ba499a7
trcmaf <anna.v.sokolova20@gmail.com>
2020-10-08 18:07:10 +0300

    init

---
+++ practice/04-practice/practicum-02-upr1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+namespace practicum_02_upr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            queue.Enqueue("First"); 
+            queue.Enqueue("Second");
+            queue.Enqueue("Third"); 
+            queue.Enqueue("Fourth");
+            while (queue.Count > 0)
+            {
+                object obj = queue.Dequeue();
+                Console.WriteLine("From Queue: {0}", obj);
+            }
+        }
+    }
+}
