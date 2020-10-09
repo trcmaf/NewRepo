de3105f4b158fdecede3eaa21e596c8c1f39bdb2
trcmaf <student@C251U37>
2020-10-09 15:31:41 +0300

    init

--- practice/05-practice/Lesson1-Serialize-People-CS/Lesson1-Serialize-People-CS/Serialize-People/Program.cs
+++ practice/05-practice/Lesson1-Serialize-People-CS/Lesson1-Serialize-People-CS/Serialize-People/Program.cs
@@ -72,9 +72,15 @@ namespace Serialize_People
         {
             Person dsp = new Person();
 
-            // TODO: Restore previously serialized Person object
-
+            // Открываем файл для чтения данных	
+            FileStream fs = new FileStream("Person.Dat", FileMode.Open);
+            // Создаем объект BinaryFormatter для выполнения десериализации
+            BinaryFormatter bf = new BinaryFormatter();
+            // Используем объект BinaryFormatter для десериализации данных из файла
+            dsp = (Person)bf.Deserialize(fs);
+            // Закрываем файл fs.Close();
             return dsp;
-        }
+      ;
+    }
     }
 }
---
+++ practice/06-practice/praktika-06/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace praktika_06
+{
+  class Person
+  {
+    //private
+    string name;
+    string surname;
+    System.DateTime birth;
+    Person(string name, string surname, DateTime birth)
+    {
+
+    }
+    Person()
+    {
+      name = "a";
+      surname = "b";
+      birth = DateTime.MinValue;
+    }
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      
+    }
+  }
+}
