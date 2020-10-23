fc011569e14aac943a6cbf7125f7ffbfa259a97f
trcmaf <student@C251U37>
2020-10-23 14:36:43 +0300

    init

---
+++ practice/08-practice/practicum-01-upr1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practicum_01_upr1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        ThreadStart starter = new ThreadStart(Counting);
+        Thread first = new Thread(starter);
+        Thread second = new Thread(starter);
+        first.Start(); 
+        second.Start();
+        first.Join(); 
+        second.Join();
+        Console.Read();
+    }
+    static void Counting()
+    {
+        for(int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine("Count: {0} - Thread' { 1} ", 1, Thread.CurrentThread, ManagedThreadld);
+            Thread.Sleep(10);
+        }
+    }
+  }
+}
