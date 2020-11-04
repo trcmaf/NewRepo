95d16063552229ffa8b5c033b7c6a0a1a75c46e5
trcmaf <anna.v.sokolova20@gmail.com>
2020-11-04 15:00:03 +0300

    init

---
+++ 08-practice/practicum-03-upr1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace practicum_03_upr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WaitCallback callback = new WaitCallback(ShowMyText);
+            ThreadPool.QueueUserWorkItem(callback, "Hello");
+            ThreadPool.QueueUserWorkItem(callback, "Hi");
+            ThreadPool.QueueUserWorkItem(callback, "Heya");
+            ThreadPool.QueueUserWorkItem(callback, "Goodbye");
+            Console.Read();
+        }
+        static void ShowMyText(object state)
+        {
+            string myText = (string)state;
+            Console.WriteLine(ThreadPool: (0) - (1), Thread.CurrentThread.ManagedThreadId, myText);
+        }
+    }
+}
+
