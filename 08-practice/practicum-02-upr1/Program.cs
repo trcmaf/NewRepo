a48ff3d4687c57c7766cce4590b17ceea55d267d
trcmaf <anna.v.sokolova20@gmail.com>
2020-11-04 14:44:45 +0300

    init

---
+++ 08-practice/practicum-02-upr1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace practicum_02_upr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mutex oneMutex = null;
+            const String MutexName = "RUNMEONLYONCE";
+            try // Пытаемся открыть Mutex
+            {
+                oneMutex = Mutex.OpenExisting(MutexName);
+            }
+            catch (WaitHandleCannotBeOpenedException)
+            {
+                // He можем открыть Mutex, потому что он не существует
+            }
+            // Создаем его, если он не существует 
+            if (oneMutex == null)
+            {
+                oneMutex = new Mutex(true, MutexName);
+            }
+            else
+            {
+                // Закрываем Mutex и выходим из приложения,
+                // так как разрешается запуск только одного его экземпляра
+                oneMutex.Close();
+                return;
+            }
+            Console.WriteLine("Our Application");
+            Console.Read();
+}
+    }
+}
